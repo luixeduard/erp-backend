da48ef90d9a21e4f2bfbaf38930711cfdfa8ac2d
edwinirg <160153816+edwinirg@users.noreply.github.com>
2025-04-29 20:34:18 -0600

    Update main.ts CORS

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,9 @@ import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors();
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ extended: true, limit: '50mb' }));
-  app
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
